d07a298a698c899a80bf09cb08d86137cb494d8b
Arsh Khan <91742287+arsh9873@users.noreply.github.com>
2022-06-11 20:29:23 +0400

    Update and rename logo.sh to viewlogos.sh

--- logo.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-for output in $(cat distro_logos.txt); do
-	echo $output
-	neofetch --ascii_distro $output --ascii_bold off -L
-done
-
---
+++ viewlogos.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+for logo in $(cat distro_logos.txt); do
+	echo $logo
+	neofetch --ascii_distro $logo --ascii_bold off -L
+done
+
